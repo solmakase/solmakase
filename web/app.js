92a724d2ef01072743152b42f5866c043ee0593d
dudgns3504 <dudgns3504@naver.com>
2025-03-24 10:04:36 +0900

    first commit

--- web/app.js
+++ web/app.js
@@ -188,6 +188,6 @@ app.post('/stop-service', async (req, res) => {
 
 
 // 서버 시작
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
